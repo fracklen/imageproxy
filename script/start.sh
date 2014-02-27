f99d5b3128a3207aaa472f5d70dbd78ddd308fcc
Martin Neiiendam <fracklen@gmail.com>
2014-02-27 11:08:40 +0100

    Allow running on peer01

--- script/start.sh
+++ script/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-/usr/sbin/varnishd -s malloc,1G -T 127.0.0.1:1080 -a 0.0.0.0:80 -F -f /etc/varnish/default.vcl &
+/usr/sbin/varnishd -s malloc,2G -T 127.0.0.1:1080 -a 0.0.0.0:80 -F -f /etc/varnish/default.vcl &
 exec bundle exec unicorn_rails -E production -c /var/www/imageproxy/release/config/unicorn.rb
 
